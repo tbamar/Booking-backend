385d246526cb51da47c9bbbf15084b51d2b21f23
Amar <amar@thoughtbins.com>
2025-09-05 15:28:34 +0530

    async await mongo

--- src/app.js
+++ src/app.js
@@ -24,9 +24,13 @@ app.options('*', cors());
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+async function connectionDB(){
+ await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
+}
+
+connectionDB();
 
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/waitinglist', waitingListRoutes);
